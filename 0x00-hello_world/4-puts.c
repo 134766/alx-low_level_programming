2893755f9448abacfd1f8d2946f2b7c3f53d7a72
134766 <amonkiptoo03@gmail.com>
2022-09-11 12:36:20 -0700

    shell hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdio.h>
+
 /**
 *main - Main function of my project
 *
-*Return: return 0 everytime
+*Return:return 0 everytime
 */
 int main(void)
 {
